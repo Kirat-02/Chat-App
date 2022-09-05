6322f3a9b1f95f522f0a805947d53d11112f8ac0
Gurkirat Singh <kiratgill999@gmail.com>
2022-09-05 22:23:56 +1000

    Login Page Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { GroupComponent } from './group/group.component';
+import { ChannelComponent } from './channel/channel.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component: LoginComponent}, {path:'group', component: GroupComponent}, {path:'channel', component: ChannelComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { GroupComponent } from './group/group.component';
+import { ChannelComponent } from './channel/channel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    GroupComponent,
+    ChannelComponent
   ],
   imports: [
     BrowserModule,
